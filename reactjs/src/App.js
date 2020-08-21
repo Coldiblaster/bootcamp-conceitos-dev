e443cdea4e27b11e0b1789d9bde0912ad07e6f94
Coldiblaster <vbastazin@gmail.com>
2020-08-21 15:13:58 -0300

    Implementando Estado e Imutabilidade

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  const [projects, setProjects] = useState([
+    "Desenvolvimento de app",
+    "Front-end web",
+  ]);
+
+  // useState retorna um array com 2 posições
+  //
+  // 1. Variável com o seu valor inicial
+  // 2. Função para atualizarmos esse valor
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
+  );
 }
 
 export default App;
