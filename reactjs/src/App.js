8f49fcf74ef4a75678f5039aa5a224bb35b5ab0b
Coldiblaster <vbastazin@gmail.com>
2020-08-22 13:37:35 -0300

    Finalizando conceitos de reactJs

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 import Header from "./components/Header";
+import api from "./services/api";
 
 function App() {
-  const [projects, setProjects] = useState([
-    "Desenvolvimento de app",
-    "Front-end web",
-  ]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
   // useState retorna um array com 2 posições
   //
   // 1. Variável com o seu valor inicial
   // 2. Função para atualizarmos esse valor
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    const response = await api.post("projects", {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Vinicius Bastazin",
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
@@ -23,7 +38,7 @@ function App() {
 
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
